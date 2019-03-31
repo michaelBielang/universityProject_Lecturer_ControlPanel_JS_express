0a8840322dca35467900b47be2dc5fe2cf14a43c
nrg <michael.bielang@hs-augsburg.de>
2019-03-31 21:14:04 +0200

    implement routing for register implement register form implement register basic layout implement db_init implement db_controller add bootstrap add some more dep

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
-var express = require('express');
-var router = express.Router();
+/**
+ * Created by Michael Bielang on 31.03.2019.
+ * www.codemerger.com
+ * bielang@codemerger.com
+ *
+ * Project:
+ * java version "10.0.1"
+ */
+var express = require('express')
+var router = express.Router()
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', function (req, res) {
+  res.render('index', {title: 'HSA Training App'})
+})
 
-module.exports = router;
+
+module.exports = router
