9cd05d7db53cf0a4996aef11fd07d5ab9eb6d43f
nrg <michael.bielang@hs-augsburg.de>
2019-04-27 13:12:30 +0200

    CSE-41 -fix remove from routing

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var cookieParser = require('cookie-parser')
 var logger = require('morgan')
 
 var indexRouter = require('./src/routes/index')
-var registerRouter = require('./src/routes/register')
+/*var registerRouter = require('./src/routes/register')*/
 
 var app = express()
 // view engine setup
@@ -19,7 +19,7 @@ app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/', indexRouter)
-app.use('/register', registerRouter)
+/*app.use('/register', registerRouter)*/
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
