c341e3893f22fdabc489f19a95264a884841c70d
Fabian Ölscher <fabian.oelscher@hs-augsburg.de>
2019-04-29 09:25:33 +0200

    Route Models to dedicated src-files
    * API for Answer, Question, Subject and Topic
    
    Resolves: db_interface_api (CSE-56)

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ var path = require('path')
 var cookieParser = require('cookie-parser')
 var logger = require('morgan')
 
-var indexRouter = require('./src/routes/index')
 /*var registerRouter = require('./routes/register')*/
 
 var app = express()
@@ -18,10 +17,7 @@ app.use(express.urlencoded({extended: false}))
 app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.use('/', indexRouter)
-/*
-app.use('/register', registerRouter)
-*/
+require('./src/routes')(app);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
