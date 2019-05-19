6d4aa71526aea174f05bc68a7841c8b27b9d5739
nrg <michael.bielang@hs-augsburg.de>
2019-05-19 11:16:55 +0200

    CSE-67
    remove console.log

--- src/routes/web/home.js
+++ src/routes/web/home.js
@@ -14,11 +14,7 @@ const session = require('express-session')
 router.get('/', async (req, res) => {
   if (session.user) {
     const subjects = await database.dbInterface.getSubjects(session.user)
-    subjects.forEach(function(element) {
-      console.log(element.dataValues);
-    });
     res.render('home', {
-      Msg: 'Welcome',
       subjects: subjects
     })
   } else {
