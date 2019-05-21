5d0cb8f95b22cb336d8499e8bd926baea93fe11c
Fabian Ölscher <fabian.oelscher@hs-augsburg.de>
2019-05-21 10:21:44 +0200

    delete not public routes

--- src/routes/api/topic.js
+++ src/routes/api/topic.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const {body, validationResult} = require('express-validator/check/index');
 const database = require('../../controller/db_controller');
 const router = express.Router();
 
@@ -15,64 +14,6 @@ router.get('/getAll/:subjectId([0-9]+)', async (req, res, next) => {
 router.get('/:id([0-9]+)', async (req, res, next) => {
     try {
         const data = await database.dbInterface.getTopic(req.params.id);
-        res.json({data});
-    } catch (e) {
-        next({message: 'Something went wrong'});
-    }
-});
-
-
-router.post('/', [
-    body('title')
-        .isLength({min: 1}),
-    body('subjectId')
-        .isLength({min: 1}),
-], async (req, res, next) => {
-    const errors = validationResult(req);
-
-    if (errors.isEmpty()) {
-        try {
-            const data = await database.dbInterface.addTopic(req.title, req.subjectId);
-            res.json({data});
-        } catch (e) {
-            next({message: 'Something went wrong'});
-        }
-    } else {
-        res.render('error', {
-            title: 'Title Error',
-            errors: errors.array(),
-            data: req.body,
-        })
-    }
-});
-
-router.post('/update', [
-    body('newTopicName')
-        .isLength({min: 1}),
-    body('topicId')
-        .isLength({min: 1}),
-], async (req, res, next) => {
-    const errors = validationResult(req);
-
-    if (errors.isEmpty()) {
-        try {
-            const data = await database.dbInterface.updateTopic(req.newTopicName, req.topicId);
-            res.json({data});
-        } catch (e) {
-            next({message: 'Something went wrong'});
-        }
-    } else {
-        res.render('error', {
-            title: 'Topic Error',
-            errors: errors.array(),
-            data: req.body,
-        })
-    }
-});
-
-router.delete('/:id([0-9]+)', async (req, res, next) => {
-    try {
-        const data = await database.dbInterface.deleteTopic(req.params.id);
         res.json({data});
     } catch (e) {
         next({message: 'Something went wrong'});
---
+++ src/routes/web/subject.js
@@ -0,0 +1,82 @@
+const express = require('express');
+const {body, validationResult} = require('express-validator/check/index');
+const database = require('../../controller/db_controller');
+const router = express.Router();
+
+router.get('/getAll/:userId([0-9]+)',  async (req, res, next) => {
+    try {
+        const data = await database.dbInterface.getSubjects(req.params.userId);
+        res.json({ data });
+    } catch (e) {
+        next({message: 'Something went wrong'});
+    }
+});
+
+router.get('/:id([0-9]+)', async (req, res, next) => {
+    try {
+        const data = await database.dbInterface.getSubject(req.params.id);
+        res.json({ data });
+    } catch (e) {
+        next({message: 'Something went wrong'});
+    }
+});
+
+router.post('/', [
+    body('subjectName')
+        .isLength({min: 1}),
+    body('userId')
+        .isLength({min: 1}),
+], async (req, res, next) => {
+    const errors = validationResult(req);
+
+    if (errors.isEmpty()) {
+        try {
+            const data = await database.dbInterface.addSubject(req.subjectName, req.userId);
+            res.json({data});
+        } catch (e) {
+            next({message: 'Something went wrong'});
+        }
+    } else {
+        res.render('Subject', {
+            title: 'Subject Error',
+            errors: errors.array(),
+            data: req.body,
+        })
+    }
+});
+
+router.post('/update', [
+    body('subjectName')
+        .isLength({min: 1}),
+    body('subjectId')
+        .isLength({min: 1}),
+], async (req, res, next) => {
+    const errors = validationResult(req);
+
+    if (errors.isEmpty()) {
+        try {
+            const data = await database.dbInterface.updateSubject(req.subjectName, req.subjectId);
+            res.json({data});
+        } catch (e) {
+            next({message: 'Something went wrong'});
+        }
+    } else {
+        res.render('Subject', {
+            title: 'Subject Error',
+            errors: errors.array(),
+            data: req.body,
+        })
+    }
+});
+
+router.delete('/:id([0-9]+)', async (req, res, next) => {
+    try {
+        const data = await database.dbInterface.deleteSubject(req.params.id);
+        res.json({data});
+    } catch (e) {
+        next({message: 'Something went wrong'});
+    }
+});
+
+
+module.exports = router;
