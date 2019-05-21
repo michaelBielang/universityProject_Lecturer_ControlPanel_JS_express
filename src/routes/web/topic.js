4f6e08f29fdcb6cf96aa1a9a93335d5ae34b0266
nrg <michael.bielang@hs-augsburg.de>
2019-05-21 14:22:09 +0200

    CSE-46
    Add topic view + functions
    Add topic edit + functions

--- src/routes/web/topic.js
+++ src/routes/web/topic.js
@@ -3,40 +3,48 @@ const {body, validationResult} = require('express-validator/check/index')
 const database = require('../../controller/db_controller')
 const router = express.Router()
 
-router.get('/getAll/:userId([0-9]+)', async (req, res, next) => {
+/**
+ * Displays all topics belonging to a particular subject
+ */
+router.get('/getAll/:subjectId([0-9]+)', async (req, res, next) => {
   try {
-    const data = await database.dbInterface.getSubjects(req.params.userId)
-    res.json({data})
+    const data = await database.dbInterface.getTopics(req.params.subjectId)
+    res.render('topic', {
+      topics: data
+    })
   } catch (e) {
     next({message: 'Something went wrong'})
   }
 })
 
-router.get('/:id([0-9]+)', async (req, res, next) => {
-  const subject = await database.dbInterface.getSubject(req.params.id)
-  res.render('edit', {
-    subject: subject[0].dataValues
+router.get('/:id([0-9]+)', async (req, res) => {
+  const topic = await database.dbInterface.getTopic(req.params.id)
+  res.render('topic_edit', {
+    topic: topic[0].dataValues
   })
 })
 
+/**
+ * Adds a new topic to the DB
+ */
 router.post('/', [
-  body('subjectName')
+  body('topicName')
     .isLength({min: 1}),
-  body('userId')
+  body('topicId')
     .isLength({min: 1}),
 ], async (req, res, next) => {
   const errors = validationResult(req)
 
   if (errors.isEmpty()) {
     try {
-      const data = await database.dbInterface.addSubject(req.subjectName, req.userId)
+      const data = await database.dbInterface.addTopic(req.body.subjectName, req.body.userId)
       res.json({data})
     } catch (e) {
       next({message: 'Something went wrong'})
     }
   } else {
-    res.render('Subject', {
-      title: 'Subject Error',
+    res.render('Topic', {
+      title: 'Topic Error',
       errors: errors.array(),
       data: req.body,
     })
@@ -44,29 +52,32 @@ router.post('/', [
 })
 
 router.post('/update', [
-  body('subjectName')
+  body('topicName')
     .isLength({min: 1}),
-  body('subjectId')
+  body('topicId')
     .isLength({min: 1}),
 ], async (req, res, next) => {
   const errors = validationResult(req)
 
   if (errors.isEmpty()) {
     try {
-      const data = await database.dbInterface.updateSubject(req.subjectName, req.subjectId)
+      const data = await database.dbInterface.updateSubject(req.body.subjectName, req.body.subjectId)
       res.json({data})
     } catch (e) {
       next({message: 'Something went wrong'})
     }
   } else {
-    res.render('Subject', {
-      title: 'Subject Error',
+    res.render('Topic', {
+      title: 'Topic Error',
       errors: errors.array(),
       data: req.body,
     })
   }
 })
 
+/**
+ * Deletes a particular topic
+ */
 router.delete('/:id([0-9]+)', async (req, res, next) => {
   try {
     const data = await database.dbInterface.deleteSubject(req.params.id)
