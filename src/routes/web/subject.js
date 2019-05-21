f1a770ce12083bce2138246b6a5b3e340bbb9b4e
nrg <michael.bielang@hs-augsburg.de>
2019-05-21 14:28:35 +0200

    CSE-46
    Merge with subject_view_47-49

--- src/routes/web/subject.js
+++ src/routes/web/subject.js
@@ -1,81 +1,71 @@
-const express = require('express');
-const {body, validationResult} = require('express-validator/check/index');
-const database = require('../../controller/db_controller');
-const router = express.Router();
+const express = require('express')
+const {body, validationResult} = require('express-validator/check/index')
+const database = require('../../controller/db_controller')
+const router = express.Router()
 
-router.get('/getAll/:userId([0-9]+)',  async (req, res, next) => {
-    try {
-        const data = await database.dbInterface.getSubjects(req.params.userId);
-        res.json({ data });
-    } catch (e) {
-        next({message: 'Something went wrong'});
-    }
-});
-
-router.get('/:id([0-9]+)', async (req, res, next) => {
-    const subject = await database.dbInterface.getSubject(req.params.id);
-    res.render('edit', {
-        subject: subject[0].dataValues
-    })
-});
+router.get('/:id([0-9]+)', async (req, res) => {
+  const subject = await database.dbInterface.getSubject(req.params.id)
+  res.render('edit', {
+    subject: subject[0].dataValues
+  })
+})
 
 router.post('/', [
-    body('subjectName')
-        .isLength({min: 1}),
-    body('userId')
-        .isLength({min: 1}),
+  body('subjectName')
+    .isLength({min: 1}),
+  body('userId')
+    .isLength({min: 1}),
 ], async (req, res, next) => {
-    const errors = validationResult(req);
+  const errors = validationResult(req)
 
-    if (errors.isEmpty()) {
-        try {
-            const data = await database.dbInterface.addSubject(req.body.subjectName, req.body.userId);
-            res.redirect('/home');
-        } catch (e) {
-            next({message: 'Something went wrong'});
-        }
-    } else {
-        res.render('Subject', {
-            title: 'Subject Error',
-            errors: errors.array(),
-            data: req.body,
-        })
+  if (errors.isEmpty()) {
+    try {
+      await database.dbInterface.addSubject(req.body.subjectName, req.body.userId)
+      res.redirect('/home')
+    } catch (exception) {
+      next({message: exception})
     }
-});
+  } else {
+    res.render('Subject', {
+      title: 'Subject Error',
+      errors: errors.array(),
+      data: req.body,
+    })
+  }
+})
 
 router.post('/update', [
-    body('subjectName')
-        .isLength({min: 1}),
-    body('subjectId')
-        .isLength({min: 1}),
+  body('subjectName')
+    .isLength({min: 1}),
+  body('subjectId')
+    .isLength({min: 1}),
 ], async (req, res, next) => {
-    const errors = validationResult(req);
+  const errors = validationResult(req)
 
-    if (errors.isEmpty()) {
-        try {
-            const data = await database.dbInterface.updateSubject(req.body.subjectName, req.body.subjectId);
-            res.redirect('/home')
-        } catch (e) {
-            next({message: 'Something went wrong'});
-        }
-    } else {
-        res.render('error', {
-            title: 'Subject Error',
-            errors: errors.array(),
-            data: req.body,
-        })
+  if (errors.isEmpty()) {
+    try {
+      await database.dbInterface.updateSubject(req.body.subjectName, req.body.subjectId)
+      res.redirect('/home')
+    } catch (exception) {
+      next({message: exception})
     }
-});
+  } else {
+    res.render('error', {
+      title: 'Subject Error',
+      errors: errors.array(),
+      data: req.body,
+    })
+  }
+})
 
 router.get('/delete/:id([0-9]+)', async (req, res, next) => {
-    try {
-        console.log(req.params.id)
-        await database.dbInterface.deleteSubject(req.params.id);
-        res.redirect('/home');
-    } catch (e) {
-        next({message: 'Something went wrong'});
-    }
-});
+  try {
+    console.log(req.params.id)
+    await database.dbInterface.deleteSubject(req.params.id)
+    res.redirect('/home')
+  } catch (exception) {
+    next({message: exception})
+  }
+})
 
-
-module.exports = router;
+module.exports = router
